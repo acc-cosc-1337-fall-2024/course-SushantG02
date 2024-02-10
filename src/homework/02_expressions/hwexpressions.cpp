ac493fa482c67e82ff93bf6e55ccaa532753a500
SushantG02 <157077888+SushantG02@users.noreply.github.com>
2024-02-10 17:50:02 +0000

    homework2

--- src/homework/02_expressions/hwexpressions.cpp
+++ src/homework/02_expressions/hwexpressions.cpp
@@ -7,7 +7,15 @@ int add_numbers(int num1, int num2)
 }
 
 //write function code here
+double get_sales_tax_amount(double meal_amount)
+{
+   double sales_tax_amount = tax_rate  * meal_amount;
+   return sales_tax_amount;
+}
 
+double get_tip_amount(double meal_amount, double tip_rate)
+{
+    double tip_amount = meal_amount * tip_rate;
+    return tip_amount;
 
-
-
+}
--- src/homework/02_expressions/main.cpp
+++ src/homework/02_expressions/main.cpp
@@ -1,11 +1,30 @@
 ﻿//write include statements
+#include "hwexpressions.h"
+#include "iostream"
 
 
 //write namespace using statement for cout
+using std::cin; using std::cout;
 
 int main()
 {
+	double meal_amount, tip_rate, tip_amount, tax_amount, total;
 	
+	cout<< "Enter meal amount: $";
+	cin>> meal_amount;
+
+	tax_amount = get_sales_tax_amount(meal_amount);
+	cout<< "Sales Tax: "<< tax_amount<<"\n";
+
+	cout<< "Enter tip rate(as a decimal): ";
+	cin>> tip_rate;
+
+	tip_amount = get_tip_amount(meal_amount, tip_rate);
+	cout<< "Tip Amount: $"<< tip_amount<<"\n";
+
+	total = meal_amount + tax_amount + tip_amount;
+
+	cout<< "Total Amount: $"<<total<<"\n";
 
 	return 0;
 }
