634d643780cad1ea46e671cf75ffb11a3cefa9ab
SushantG02 <157077888+SushantG02@users.noreply.github.com>
2024-02-20 23:48:53 +0000

    Homework 3

--- src/homework/03_decisions/decisions.cpp
+++ src/homework/03_decisions/decisions.cpp
@@ -1,4 +1,62 @@
 //write include statement for decisions header
-
+#include "decisions.h"
+using std::string;
 
 //Write code for function(s) code here
+
+ std:: string get_letter_grade_using_if(int grade){
+   std:: string result = "";
+
+    if (grade >= 90 and grade <= 100)
+    {
+        result = "A";
+    }
+    
+    else if (grade >= 80 and grade <= 89)
+    {
+        result = "B";
+    }
+   
+    else if (grade >= 70 and grade <= 79)
+    {
+        result = "C";
+    }
+
+    else if (grade >= 60 and grade <= 69)
+    {
+        result = "D";
+    }
+
+    else 
+    {
+        result = "F";
+    }
+   return result;
+
+
+}
+
+std:: string get_letter_grade_using_switch(int grade)
+
+{
+    switch (grade / 10)
+    {
+        case 10:
+        case 9:
+            return "A";
+            break;
+        case 8:
+            return "B";
+            break;
+        case 7:
+            return "C";
+            break;
+        case 6:
+            return "D";
+            break;
+        default:
+            return "F";
+
+    }
+
+}
--- src/homework/03_decisions/main.cpp
+++ src/homework/03_decisions/main.cpp
@@ -1,6 +1,20 @@
 //write include statements
-
+#include "decisions.h"
+#include <iostream>
 int main() 
 {
+	int grade;
+	std::cout<<"Enter your numerical from grade (0 to 100):  ";
+	std::cin>> grade;
+	
+	if (grade < 0 or grade > 100) 
+	{
+		std::cout<<"Error: grade must be between 0 and 100."<<"\n";
+	}
+	else 
+	{
+	std::cout<<"using if: "<< get_letter_grade_using_if(grade)<<"\n";
+	std::cout<<"using switch: "<< get_letter_grade_using_switch(grade)<<"\n";
+	}
 	return 0;
 }
