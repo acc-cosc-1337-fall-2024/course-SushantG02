0771479a4fec23fb49f8205ae0f9b7b383b0855e
SushantG02 <157077888+SushantG02@users.noreply.github.com>
2024-02-24 23:11:52 +0000

    Homework 4

--- src/homework/04_repetition/main.cpp
+++ src/homework/04_repetition/main.cpp
@@ -1,8 +1,52 @@
 //write include statements
-
+#include"repetition.h"
+#include<iostream>
 //write using statements
+using std::cout;
+using std::cin;
+
 
 int main() 
 {
+	int choice;
+
+	do{
+		cout<<"Math Menu:"<<"\n";
+		cout<<"1- Factorial"<<"\n";
+		cout<<"2- Greatest Common Divisor"<<"\n";
+		cout<<"3- Quit"<<"\n";
+		cout<<"Enter your choice: ";
+		cin>> choice;
+	
+		switch (choice)
+		{
+			case 1: {
+				int num;
+				cout<<"Enter a number to calulate factorial: ";
+				cin>>num;
+				cout<<"Factorial of "<<num<<" is "<<factorial(num)<<"\n";
+				break;
+			}
+			case 2: {
+				int num1, num2;
+				cout<<"Enter two numbers to find GCD: ";
+				cin>> num1>>num2;
+				cout<<"GCD of "<<num1<<" and "<<num2<<" is "<<gcd(num1,num2)<<"\n";
+				break;
+			}
+			case 3: {
+				char confirm ;
+				cout<<"Are you sure you want to exit? (y/n:)";
+				cin>> confirm;
+				if (confirm == 'y')
+					cout<<"Exiting the system. Thank you for trying!"<<"\n";
+				break;
+			}
+			default: 
+			cout<<"Invlaid choice. Try again."<<"\n";
+		}	
+	
+	} while (choice != 3);
 	return 0;
+
 }
--- src/homework/04_repetition/repetition.cpp
+++ src/homework/04_repetition/repetition.cpp
@@ -1,3 +1,29 @@
 //add include statements
-
+#include"repetition.h"
+#include <iostream>
 //add function(s) code here
+int factorial(int num)
+{
+    int result = 1;
+    while(num > 0) 
+    {
+        result *= num;
+        num--;
+    }
+return result;
+}
+
+int gcd(int num1, int num2)
+{
+    while(num1 != num2) 
+    {
+        if (num1<num2)
+            std::swap(num1,num2);
+        if (num1>num2)
+            num1 -= num2;
+    }
+    return num1;
+
+}
+
+
