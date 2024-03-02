d7ff262bdc91d4cff1ad7f43698d3de6bce596fb
SushantG02 <157077888+SushantG02@users.noreply.github.com>
2024-03-02 23:39:58 +0000

    Homework 5

--- src/homework/05_functions/func.cpp
+++ src/homework/05_functions/func.cpp
@@ -1,3 +1,54 @@
 //add include statements
-
+#include "func.h"
+#include<iostream>
 //add function code here
+using std::string;
+
+
+double get_gc_content(const std::string& dna) 
+{
+    int count = 0;
+    for(char nucleotide : dna) 
+    {
+        if(nucleotide == 'C' or nucleotide == 'G') 
+        {
+            count++;
+        }
+    }
+    return static_cast<double>(count) / dna.length();
+}
+
+std::string get_reverse_string(const std::string& dna)
+{
+    std::string reversed;
+    for (int i = dna.length() - 1; i >= 0; --i) 
+    {
+        reversed += dna[i];
+    }
+    return reversed;
+}
+
+std::string get_dna_complement(const std::string& dna) 
+{
+    std::string reverse_complement = dna;
+    for (char& nucleotide : reverse_complement) 
+    {
+        switch (nucleotide) 
+        {
+            case 'A':
+                nucleotide = 'T';
+                break;
+            case 'T':
+                nucleotide = 'A';
+                break;
+            case 'C':
+                nucleotide = 'G';
+                break;
+            case 'G':
+                nucleotide = 'C';
+                break;
+        }
+    }
+    // Reverse the complemented string before returning it
+    return get_reverse_string(reverse_complement);
+}
--- src/homework/05_functions/main.cpp
+++ src/homework/05_functions/main.cpp
@@ -1,6 +1,48 @@
+#include "func.h"
+#include<iostream>
+using std::cout; using std::cin;
 
 int main() 
 {
+	int option;
+	std::string dna;
+	do 
+	{
+		cout<<"Menu"<<"\n";
+		cout<<"1- Get GC Content"<<"\n";
+		cout<<"2- Get Reversed"<<"\n";
+		cout<<"3- Get DNA Complement"<<"\n";
+		cout<<"4- Exit"<<"\n";
+		cout<<"Choose one option: ";
+		cin>>option;
+
+		switch (option)
+		{
+			case 1: 
+				cout<<"Enter DNA string: ";
+				cin>>dna;
+				cout<<"GC Content: "<<get_gc_content(dna)<<"\n";
+				break;
+			case 2: 
+				cout<<"Enter DNA string: ";
+				cin>>dna;
+				cout<<"Reversed: "<<get_reverse_string(dna)<<"\n";
+				break;
+			case 3:
+				cout<<"Enter DNA string: ";
+				cin>>dna;
+				cout<<"DNA Complement: "<<get_dna_complement(dna)<<"\n";
+				break;
+			case 4:
+				cout<<"Exiting program "<<"\n";
+				break;
+			default: 
+				cout<<"Invalid option, Please try agian."<<"\n";
+			
+
+		}
+		
+	} while (option != 4);
 
 	return 0;
 }
